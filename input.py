5cdf8d55342c39c0b7843bd92c06bd9bb729087f
Twinkltos1993 <Twinkltos@gmail.com>
2022-07-14 18:36:13 +0300

    print and input

---
+++ input.py
@@ -0,0 +1,28 @@
+#input
+print('What is you name?')
+name = input()
+print('Hello, ', name )
+
+#exmaple1
+name = input()
+print(name , "- чемпион!")
+
+#example2
+name = input()
+print("Привет," , name)
+
+#example3
+name = input()
+born = input()
+this = input()
+print(name)
+print(born)
+print(this)
+
+#example4
+hello = input()
+it = input()
+me = input()
+print(me)
+print(it)
+print(hello)
