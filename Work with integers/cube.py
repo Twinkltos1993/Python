5233011756b220b6d1af031a9bdd908e1d93ce65
Twinkltos1993 <Twinkltos@gmail.com>
2022-07-15 17:24:57 +0300

    integers

---
+++ Work with integers/cube.py
@@ -0,0 +1,5 @@
+a = int(input())
+S = 6 * a**2
+V = a**3
+print('Size =', V)
+print('Space =', S)
---
+++ Work with integers/integers.py
@@ -0,0 +1,53 @@
+# Integer data type
+# Conversion of term to whole number
+# Operations on integers
+# Problem solving
+
+# example
+numb1 = 7
+numb2 = 10
+numb3 = numb1 + numb2
+print(numb3)
+
+# example1
+a = 3
+b = 2
+print(a + b)
+print(a - b)
+print(a * b)
+print(a / b)
+
+# example2
+num1 = 2 + 3 * 4
+num2 = (2 + 3) * 4
+print(num1)
+print(num2)
+
+# example3
+s = '1992'
+year = int(s)
+print(year)
+
+# example4
+num1 = int(input())
+num2 = int(input())
+print(num1 + num2)
+
+# example5
+a = int(input())
+b = a + 1
+c = a + 2
+print(a)
+print(b)
+print(c)
+
+# example6
+num1 = int(input())
+num2 = int(input())
+num3 = int(input())
+print(num1 + num2 + num3)
+
+# or
+
+num1, num2, num3 = int(input()), int(input()), int(input())
+print(num1 + num2 + num3)
