ac4a4ab5f07c1a97f5e5e511eb9d2894275fdce5
twinkltos <twinkltos1993@gmail.com>
2022-07-17 22:48:59 +0300

    so sad

--- Work with integers/functions.py
+++ Work with integers/functions.py
@@ -33,3 +33,40 @@ print(g)
 #example4
 x = int(input())
 print(x, x*2, x*3, x*4, x*5, sep ='---')
+
+#example5
+b1 = int(input())
+q = int(input())
+n = int(input())
+f = b1*q**(n-1)
+print(f)
+
+#example6
+a = int(input())
+b = (a // 100)
+print(b)
+
+#example7
+n = int(input())
+k = int(input())
+d = (k // n)
+f = (k % n)
+print(d)
+print(f)
+
+#exmaple8
+n = int(input())
+print(n // 2 + n % 2)
+
+#example9
+n = int(input())
+k = -n // 4
+print(-k)
+
+
+#example10
+h = int(input())
+d = (h // 60)
+a = (h % 60)
+print(h, 'min - it', d, 'hours', a, 'minutes.')
+
