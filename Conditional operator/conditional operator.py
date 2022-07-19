e2dbfc3218b8268c92b964ed5bc776f557e9ec75
Twinkltos1993 <Twinkltos@gmail.com>
2022-07-19 20:34:31 +0300

    how long

--- Conditional operator/conditional operator.py
+++ Conditional operator/conditional operator.py
@@ -75,3 +75,78 @@ if a > d < b:
         print(d)
         
 #example 8
+age = int(input())
+if age <= 13:
+    print('childhood')
+if 14 <= age <= 24:
+    print('youth')
+if 25 <= age <= 59:
+    print('maturity')
+if age >= 60:
+    print('age')
+
+# example 9
+a = int(input())
+b = int(input())
+c = int(input())
+sum = 0
+if a > sum < b and c > sum:
+    print(a+b+c)
+elif a < sum < b and c > sum:
+    print(b+c)
+elif a > sum > b and c > sum:
+    print(a+c)
+elif a > sum < b and c < sum:
+    print(a+b)
+elif a > sum > b and c < sum:
+    print(a)
+elif a < sum < b and c < sum:
+    print(b)
+elif a < sum > b and c > sum:
+    print(c)
+elif a < sum > b and c < sum:
+    print(sum)
+    
+# example 10
+x = int(input())
+if -1 < x < 17:
+    print('True')
+else:
+    print('False')
+
+# example 11
+x = int(input())
+if x <= -3 or x >= 7:
+    print("True")
+else:
+    print("False")
+    
+# example 12
+x = int(input())
+if -2 >= x > -30 or 7 < x <= 25:
+    print('True')
+else:
+    print('False')
+
+# example 13
+a = int(input())
+if a >= 1000 and a <= 9999 and (a % 7 == 0 or a % 17 == 0):
+    print('YES')
+else:
+    print("NO")
+
+# example14
+a = int(input())
+b = int(input())
+c = int(input())
+if a + b > c and a + c > b and b + c > a:
+    print('YES')
+else:
+    print('NO')
+    
+# example15
+a = int(input())
+if a % 4 == 0 and a % 100 != 0 or a % 400 == 0:
+    print('YES')
+else:
+    print('NO')
